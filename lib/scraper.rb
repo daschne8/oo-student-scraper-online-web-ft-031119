35401190dd22b2c3ec2bb1038baa6211f39609c6
Daniel Schneider <daschne8@gmail.com>
2019-03-30 18:24:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,14 +22,28 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
+
     student_file = {
-      twitter: doc.css("div.social-icon-container a")[0]['href'],
-      linkedin: doc.css("div.social-icon-container a")[1]['href'],
-      github: doc.css("div.social-icon-container a")[2]['href'],
-      blog: doc.css("div.social-icon-container a")[3]['href'],
+      # twitter: doc.css("div.social-icon-container a")[0]['href'],
+      # linkedin: doc.css("div.social-icon-container a")[1]['href'],
+      # github: doc.css("div.social-icon-container a")[2]['href'],
+      # blog: doc.css("div.social-icon-container a")[3]['href'],
       profile_quote: doc.css("div.profile-quote").text.strip,
       bio: doc.css("div.description-holder p").text
     }
+    doc.css("div.social-icon-container a").each do |anchor|
+      case a = anchor['href']
+      when a.include?('twit')
+        student_file[:twitter] = a
+      when a.include?('link')
+        student_file[:linkedin] = a
+      when a.include?('git')
+        student_file[:github] = a
+      else
+         student_file[:blog] = a
+      end
+
+    end
     student_file
   end
 
